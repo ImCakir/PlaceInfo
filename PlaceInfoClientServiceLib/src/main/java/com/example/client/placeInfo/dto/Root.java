0ed8d18c6a4629323c926f35203b2881dac948db
Onur Özcan <119119881+imozcan@users.noreply.github.com>
2025-06-24 01:13:18 +0300

    Added PlaceInfoClientService

---
+++ PlaceInfoClientServiceLib/src/main/java/com/example/client/placeInfo/dto/Root.java
@@ -0,0 +1,12 @@
+package com.example.client.placeInfo.dto;
+
+
+import lombok.Data;
+import java.util.List;
+
+@Data
+
+public class Root {
+    public List<WikiSearch> listWikiSearch;
+
+}
---
+++ PlaceInfoClientServiceLib/src/test/java/com/example/client/placeInfo/AppTests.java
@@ -0,0 +1,12 @@
+package com.example.client.placeInfo;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+@Disabled
+class AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
