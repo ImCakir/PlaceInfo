341defbfcd2498ae3c79ad339a213673370a3515
Onur Özcan <119119881+imozcan@users.noreply.github.com>
2025-06-24 02:36:49 +0300

    Update Root.java

--- PlaceInfoClientServiceLib/src/main/java/com/example/client/placeInfo/dto/Root.java
+++ PlaceInfoClientServiceLib/src/main/java/com/example/client/placeInfo/dto/Root.java
@@ -1,12 +1,15 @@
 package com.example.client.placeInfo.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import java.util.List;
 
 @Data
 
+
 public class Root {
+    @JsonProperty("geonames")
     public List<WikiSearch> listWikiSearch;
 
 }
