9efbc7d4ad01066c4092d5bc8468916238420b45
onur <onuraozcan@gmail.com>
2025-07-01 19:07:07 +0300

    update

--- PlaceInfoApplication/src/main/java/com/example/PlaceInfoApplication/Controller/PlaceInfoController.java
+++ PlaceInfoApplication/src/main/java/com/example/PlaceInfoApplication/Controller/PlaceInfoController.java
@@ -1,17 +1,36 @@
 package com.example.PlaceInfoApplication.Controller;
 
+import com.example.client.placeInfo.PlaceInfoClient;
+import com.example.client.placeInfo.dto.Root;
+import com.example.client.placeInfo.dto.WikiSearch;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/places")
+@RequestMapping("/dev/api")
 public class PlaceInfoController {
-    private PlaceInfoClient m_placeInfoClient;
+    private final PlaceInfoClient m_placeInfoClient;
 
     public PlaceInfoController(PlaceInfoClient placeInfoClient)
     {
         m_placeInfoClient = placeInfoClient;
     }
+    @GetMapping("place")
+    public WikiSearch getPlace(@RequestParam String q)
+    {
+        return m_placeInfoClient.getWikiSearchInSingle(q);
+    }
+    @GetMapping("details")
+    public Iterable<WikiSearch>  getPlaceInList(@RequestParam String q)
+    {
+        return m_placeInfoClient.getWikiSearchInList(q);
+    }
 
-
+    @GetMapping("demo")
+    public Root getPlaceRoot(@RequestParam String q)
+    {
+        return m_placeInfoClient.getWikiSearch(q);
+    }
 }
--- PlaceInfoApplication/src/main/java/com/example/PlaceInfoApplication/PlaceInfoApplication.java
+++ PlaceInfoApplication/src/main/java/com/example/PlaceInfoApplication/PlaceInfoApplication.java
@@ -2,7 +2,11 @@ package com.example.PlaceInfoApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "com.example")
+@EntityScan(basePackages = "com.example")
 @SpringBootApplication
 public class PlaceInfoApplication {
 
